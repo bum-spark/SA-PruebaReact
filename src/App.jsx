0ede14974cc681fa94824a61044048f67f1d7759
Jordan Cazares <l.caej040411@itses.edu.mx>
2026-01-26 14:49:00 -0600

    Props

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import Header from './Components/Header.jsx'
+import Card from './Components/Card.jsx'
+import Footer from './Components/Footer.jsx'
+import './App.css'
+
+//import { useState, useEffect, useReducer } from 'react'
+//useState: Una variable va a estar renderizando cuando se utilice su metodo
+//useEffect: esperar un resultado y luego utilizar las variables
+
+import { useState, useEffect } from 'react'
+import { db } from './db/db.js'
+
+
+function App() {
+  //     variable   metodo para        valor inicial
+  //                actualizar 
+  //               la variable
+  /*const [customer, setCustomer] = useState({});
+  const [total, setTotal] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [modal, setModal] = useState(false);
+
+  ()=>setTotal(total + 1)*/
+  
+  /*if(auth){
+    const [modal, setModal] = useState(false);
+  }*/ 
+  //Esto no se puede hacer dentro de un condicional o ciclo, los hooks se deben definir al iniicio
+
+  /*console.log(customer);
+  console.log(total);
+  console.log(products);
+  console.log(modal);*/
+
+  const [data, setData] = useState(db);
+
+  console.log(data);
+
+  /*useEffect( ()=> {
+    setData(db)
+  }, [] );*/
+  //Al momento que el db cambie, se va a ejecutar el useEffect
+
+  return (
+    <div>
+      <Header/>
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+
+          {data.map(
+            (guitar) => (
+              <Card
+                key={guitar.id}
+                guitar={guitar}
+              />
+            ) 
+          )}
+
+        </div>
+      </main>
+
+      <Footer/>
+
+    </div>
+
+  )
+}
+
+export default App
